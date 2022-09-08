deb9789fba3e61bbed8b4dba2bf3f146c18bea1a
Nicopopxd <nicopop.xd+git@gmail.com>
2022-09-08 14:16:16 -0400

    Ajout des autre classe de Type INavire

---
+++ BatailleNavaleWPF/Patrouilleur.cs
@@ -0,0 +1,24 @@
+﻿using BatailleNavale;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BatailleNavaleWPF
+{
+    internal class Patrouilleur : INavire
+    {
+        public bool Coule { get => EstCoule(); }
+        private readonly Case[] Cases;
+
+        private bool EstCoule()
+        {
+            foreach (Case carre in Cases)
+            {
+                if (!carre.Touche) return false;
+            }
+            return true;
+        }
+    }
+}
