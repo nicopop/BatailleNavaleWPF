d5b67c37d4999f2eeb6e63d9281c70b1e6c3d734
Nicopopxd <nicopop.xd+git@gmail.com>
2022-09-08 14:09:36 -0400

    Ajoute de l'interface INavire.cs et de la premiere instance SousMarin.cs

---
+++ BatailleNavaleWPF/INavire.cs
@@ -0,0 +1,14 @@
+﻿using BatailleNavale;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BatailleNavaleWPF
+{
+    internal interface INavire
+    {
+        bool Coule { get; }
+    }
+}
